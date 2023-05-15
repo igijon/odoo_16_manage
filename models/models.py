cdcfe5752e3092ed48ef5c6da9206567dfd01a49
Inma Gijón <igijoninf@gmail.com>
2023-05-15 11:00:39 +0200

    Campos básicos y relacionales

--- models/models.py
+++ models/models.py
@@ -7,12 +7,40 @@ class task(models.Model):
     _name = 'manage.task'
     _description = 'manage.task'
 
+    name = fields.Char(string="Nombre", readonly=False, required=True, help="Introduzca el nombre") #Name
+    description = fields.Text()
+    creation_date = fields.Date()
+    start_date = fields.Datetime()
+    end_date = fields.Datetime()
+    is_paused = fields.Boolean()
+    sprint = fields.Many2one("manage.sprint", ondelete="set null", help="Sprint relacionado")
+    technologies = fields.Many2many(comodel_name="manage.technology",
+                                    relation_name="technologies_tasks",
+                                    column1="task_id",
+                                    column2="technology_id")
+
+
+class sprint(models.Model):
+    _name = 'manage.sprint'
+    _description = 'manage.sprint'
+
     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    description = fields.Text()
+    start_date = fields.Datetime()
+    end_date = fields.Datetime()
+    tasks = fields.One2many(string="Tareas", comodel_name="manage.task", inverse_name='sprint')
+
+
+class technology(models.Model):
+    _name = 'manage.technology'
+    _description = 'manage.technology'
+
+    name = fields.Char()
+    description = fields.Text()
+
+    photo = fields.Image(max_width=200, max_height=200)
+    tasks = fields.Many2many(comodel_name="manage.task",
+                             relation_name="technologies_tasks",
+                             column1="technology_id",
+                             column2="task_id")
+
